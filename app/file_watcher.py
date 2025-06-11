245127eb39fd153eddbc9f26f21fbab58ed9d504
Hussain <134604913+sathamhussain123@users.noreply.github.com>
2025-06-12 00:39:54 +0530

    Update file_watcher.py

--- app/file_watcher.py
+++ app/file_watcher.py
@@ -2,7 +2,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import requests, time, os
 
-WATCH_DIR = "/data/projects/auth-service"
+WATCH_DIR = "/app/data/projects"
 
 class JenkinsfileHandler(FileSystemEventHandler):
     def on_modified(self, event):
